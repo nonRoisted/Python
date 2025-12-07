6e92f43125c6fec8d282d9c94eaa6c33fa2abd9c
nonRoisted <leo.lushnikov@yandex.ru>
2025-12-07 18:52:56 +0300

    Add files via upload

---
+++ task_76.py
@@ -0,0 +1,29 @@
+import urllib.request
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+file = urllib.request.urlopen("https://raw.githubusercontent.com/dm-fedorov/python_basic/master/data/opendata.stat").read().decode('utf-8').split("\n")[1:]
+
+pension = []
+date_y = []
+for st in file:
+    st = st.split(",")
+    if st[0] == "Средняя пенсия":
+        if st[1] == 'Забайкальский край':
+            date=st[2].split("-")
+            if date[0] == '2018':
+                pension.append(int(st[3]))
+                date_y.append(int(date[1]))
+
+if len(pension) <= 12:
+    print(f"Внимание следующие данные представлены для {len(pension)} месяцев")
+else:
+    print(f"Внимание следующие данные представлены для {len(pension)} месяцев")
+print(f"Средняя пенсия в Забайкальском крае за 2018 год составляет: {round(sum(pension)/len(pension),1)}")
+
+plt.plot(date_y, pension) #Построение линейного графика
+plt.title("График изменения средней пенсии") #Добавление заголовка
+plt.ylabel("Cредняя пенсия за месяц") #Подпись для оси y
+plt.xlabel("Месяц") #для x
+plt.grid(True) #Добавил сетку
+plt.show() #Показ графика
